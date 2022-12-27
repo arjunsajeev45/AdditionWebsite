034b4e327dba9703b37823794b0514496c53dbef
speehive <speehive@DESKTOP-TPF0JOE>
2022-12-27 16:28:55 +0530

    Add project files.

---
+++ Controllers/AddController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdditionWebsite.Controllers
+{
+    public class AddController : Controller
+    {
+        public IActionResult Index()
+        {
+ 
+            return View();
+        }
+
+        public IActionResult AddDisplay(int a,int b)
+        {
+           int ans = a+b;
+           ViewBag.Ans1=ans;
+
+
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdditionWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
